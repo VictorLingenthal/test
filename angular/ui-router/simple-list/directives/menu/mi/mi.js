1f0e69bc62e3ffb338cc6fcead70da1573174d4a
lingenthalvictor <victor.lingenthal@gmail.com>
2014-09-16 22:23:01 +0900

    Simple List Drag Working

--- angular/ui-router/simple-list/directives/menu/mi/mi.js
+++ angular/ui-router/simple-list/directives/menu/mi/mi.js
@@ -1,32 +1,34 @@
 angular.module('uiRouterSample')
 
-    .directive('diMenu', function () {
+    .directive('diMi', function () {
         return {
             restrict: 'E',
-            templateUrl: "directives/menu/menus.html",
+            templateUrl: "directives/menu/mi/mi.html",
             replace: true,
             controller: function($scope, $element, $state) {
 
-                var drop1 = new BetaJS.UI.Interactions.Drop($('.page1'), {
+                var drop = new BetaJS.UI.Interactions.Drop($element, {
                     enabled: true,
                     droppable: function () {
                         return true;
                     }
                 });
-                drop1.on('hover', function (dr) {
+                drop.on('hover', function (dr) {
                     dr.modifier.css('background-color', 'green');
                     console.log(dr);
-                    $state.go('main.page1');
+                    $state.go('main.' + $scope.name);
                 });
-                drop1.on("hover-invalid", function (dr) {
+                drop.on("hover-invalid", function (dr) {
                     dr.modifier.css("background-color", "gray");
                 });
-                drop1.on("dropped", function () {
+                drop.on("dropped", function () {
                     //alert("yeah");
                 });
 
             },
-            scope: {},
+            scope: {
+                name: "@"
+            },
             link: function(scope) {}
         };
     });
